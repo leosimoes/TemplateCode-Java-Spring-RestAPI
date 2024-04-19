407597e0a677e9091370219233fbcdd3901c6fb2
Leonardo <leosimoes.engcomp@gmail.com>
2024-04-19 15:17:57 -0300

    Project started

---
+++ src/main/java/com/project/restapi/TemplateCodeJavaSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateCodeJavaSpringRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateCodeJavaSpringRestApiApplication.class, args);
+    }
+
+}
